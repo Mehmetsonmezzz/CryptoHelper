bd731616aeb7a0817438769ce89194c95deed6aa
Mehmet <mehmetsonmez216@gmail.com>
2024-01-22 02:52:46 +0300

    git init

---
+++ src/main/java/com/workintech/CryptoHelper/CryptoHelperApplication.java
@@ -0,0 +1,13 @@
+package com.workintech.CryptoHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoHelperApplication.class, args);
+	}
+
+}
